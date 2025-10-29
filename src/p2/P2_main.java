71f4af1f3796f474ffcf65f70bcfc5d85f45a243
Benn Tschierschke <benn@MacBook-Pro-von-Benn.local>
2025-10-29 13:01:59 +0100

    Präsenzaufgabe 3: i,j,k miteinander vergleichen

--- src/p2/P2_main.java
+++ src/p2/P2_main.java
@@ -5,15 +5,14 @@ public class P2_main {
 	public static void main(String[] args) {
 		int i;
 		i = 18;
-		
+
 		boolean b;
 		b = false;
-		
-		if (i >10) {
+
+		if (i > 10) {
 			b = true;
 		}
 		System.out.println(b);
-				
 
 	}
 
