c862a501d33cb3ae4e645413dc094ebb455c76d5
Benn Tschierschke <benn@MacBook-Pro-von-Benn.local>
2025-10-29 13:03:34 +0100

    Präsenzaufgabe 3: i,j,k miteinander vergleichen

--- src/p3/P3_main.java
+++ src/p3/P3_main.java
@@ -3,7 +3,19 @@ package p3;
 public class P3_main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int i = 4;
+		int j = 7;
+		int k = 12;
+
+		if (i < j) {
+			System.out.println("A");
+		}
+		if (i < k) {
+			System.out.println("B");
+		}
+		if (j < k) {
+			System.out.println("C");
+		}
 
 	}
 
